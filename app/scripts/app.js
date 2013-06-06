fc3af9f2981803d8fad53e8e42c761271a55beb8
davidMGS <david.ho@mountaingapsolutions.com>
2013-06-06 17:48:23 -0400

    - add components to index - update message

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,10 @@
 'use strict';
 
-angular.module('devfitApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('devfitApp', []).config(function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'views/main.html',
+		controller : 'MainCtrl'
+	}).otherwise({
+		redirectTo : '/'
+	});
+});
